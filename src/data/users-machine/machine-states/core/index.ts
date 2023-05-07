9fc9855ab1f4e7902297613637c69ae40372fb52
Eran Sakal <public@sakalim.com>
2023-05-07 14:15:28 +0300

    cleanups

--- src/data/users-machine/machine-states/core/index.ts
+++ src/data/users-machine/machine-states/core/index.ts
@@ -1,11 +1,10 @@
-import { UsersMachineContext, UsersMachineStateConfig } from '../../types';
+import { UsersMachineStateConfig } from '../../types';
 import { bootUpState } from './boot-up-state';
 import { operatingState } from './operating-state';
-import { actions } from 'xstate';
+
 
 export const coreState: UsersMachineStateConfig = {
   initial: 'bootUp',
-
   states: {
     bootUp: bootUpState,
     operating: operatingState,
